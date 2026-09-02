e3307f485ad4698c5e2f2a0a83ba91d79e818109
BenjaF33 <benjalucasfabris@gmail.com>
2026-09-02 08:23:11 -0300

    hola

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,22 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Footer } from './compartidos/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('MuseoVirtual');
+  protected readonly menuAbierto = signal(false);
+
+  protected alternarMenu() {
+    this.menuAbierto.update((abierto) => !abierto);
+  }
+
+  protected cerrarMenu() {
+    this.menuAbierto.set(false);
+  }
 }
